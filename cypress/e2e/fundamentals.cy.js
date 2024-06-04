6883ef87beb91b9d587cdbe3c968114c76d5f295
Dominika Wojewska <d.n.wojewska@gmail.com>
2024-06-04 17:30:38 +0200

    write a test that checks header text

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,5 +1,20 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+// describe block: takes two arguments. 
+// 1) string, describing the test file
+// 2) callback function that contains all the tests
+
+describe('fundamentals test', () => {
+  // it block: for individual tests (unit tests?) for the describe block
+  // same syntax as for the describe block
+  // there can be many it() blocks
+  it('Contains correct header text', () => {
+    // cy object
+    // has seceral methods: 
+    // visit - navigates to particular page
+    // click - simulates a click event
+    // type - simulates typing
+    // check the docs for more: https://docs.cypress.io/guides/core-concepts/interacting-with-elements#Actionability
+    cy.visit('/fundamentals')
+    cy.get('[data-test="fundamentals-header"]').contains(/Testing Fundamentals/i) //make it case-insensitive, that's why it's in a regex. It could also be a string for direct matching
+    cy.get('[data-test="fundamentals-header"]').should('contain.text', 'Testing Fundamentals')
   })
 })
